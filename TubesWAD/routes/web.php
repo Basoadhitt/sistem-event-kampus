e681d82cc1fb5021ff2d101fd4cc1a05cecaec85
Basoadhitt <Vanzzgameplay76@gmail.com>
2026-01-03 19:20:32 +0700

    Update routes dan seeder

--- TubesWAD/routes/web.php
+++ TubesWAD/routes/web.php
@@ -2,5 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\RegistrasiController;
 
-Route::resource('events', EventController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+require __DIR__.'/auth.php';
+
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::resource('events', EventController::class);
+});
+
+
+Route::middleware(['auth', 'role:peserta'])->group(function () {
+    Route::get('/registrasi/create', [RegistrasiController::class, 'create'])
+        ->name('registrasi.create');
+
+    Route::post('/registrasi', [RegistrasiController::class, 'store'])
+        ->name('registrasi.store');
+});
