78ed5162df1e1f512da07f32c05c5b7b6211607d
Basoadhitt <Vanzzgameplay76@gmail.com>
2026-01-03 15:28:13 +0700

    Menambahkan migration tabel events

---
+++ TubesWAD/database/migrations/0001_01_01_000000_create_events_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_event');
+            $table->text('deskripsi')->nullable();
+            $table->date('tanggal_event');
+            $table->string('lokasi');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
